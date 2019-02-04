4530ab23aae163abc052044a98eac8ef6faab2b3
Hassan <hpez58@gmail.com>
2019-02-04 11:49:04 +0330

    Changed travis before_script

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget http://www.us.apache.org/dist/lucene/solr/6.0.1/solr-6.0.1.tgz
-tar xzf solr-6.0.1.tgz solr-6.0.1/bin/install_solr_service.sh --strip-components=2
-sudo ./install_solr_service.sh solr-6.0.1.tgz
+wget http://mirror.23media.de/apache/lucene/solr/7.6.0/solr-7.6.0.tgz
+tar xzf solr-7.6.0.tgz solr-7.6.0/bin/install_solr_service.sh --strip-components=2
+sudo ./install_solr_service.sh solr-7.6.0.tgz
 sudo /opt/solr/bin/solr create -c colletion1 -n data_driven_schema_configs
