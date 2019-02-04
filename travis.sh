8253b5ba787671d1a4206cdae810ba207a1e3fcd
Hassan <hpez58@gmail.com>
2019-02-04 14:46:06 +0330

    Updated travis.sh

--- travis.sh
+++ travis.sh
@@ -3,4 +3,5 @@
 wget http://mirror.23media.de/apache/lucene/solr/7.6.0/solr-7.6.0.tgz
 tar xzf solr-7.6.0.tgz solr-7.6.0/bin/install_solr_service.sh --strip-components=2
 sudo ./install_solr_service.sh solr-7.6.0.tgz
-su - solr -c "/opt/solr/bin/solr create -c colletion1 -n data_driven_schema_configs"
+sudo -u solr
+/opt/solr/bin/solr create -c colletion1 -n data_driven_schema_configs
